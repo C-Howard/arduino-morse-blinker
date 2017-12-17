5795ff22fba087f68f3e163a5f2a2bc7e55a61c9
Cristyn Howard <cristyn.howard@mail.utoronto.ca>
2017-12-16 23:33:33 -0500

    Added constants/variables, wrote setup.

--- morse_blinker.ino
+++ morse_blinker.ino
@@ -1,6 +1,17 @@
-void setup() {
-  // put your setup code here, to run once:
+const int outPin = 3;
+const int blinkTime = 200; // length of a blink unit, in ms
+const int maxLength = 25; // max message length, arbitrary
+boolean msgSent = false;
 
+//   Write your message to be relayed here!
+//     Only a-z, A-Z, and spaces.
+//     No puntuation, special chars, or numbers.
+
+char message[] = "you are a genius"; 
+
+
+void setup() {
+  pinMode(outPin, OUTPUT);
 }
 
 void loop() {
